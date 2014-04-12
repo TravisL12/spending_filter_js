86f44e4c9610891d24a66a27cc65e59f672d34d0
Travis Lawrence <travis.lawrence12@gmail.com>
2014-04-12 12:05:03 -0700

    add pagination

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,22 @@
 var spendingApp = angular.module('spendingApp', []);
 
-spendingApp.controller('SpendCtrl', ['$scope', '$http', '$filter', function($scope, $http, $filter){
+// spendingApp.factory BUILD FACTORY FOR PAGINATION CONTROL!!!
+
+spendingApp.controller('SpendCtrl', ['$scope', '$http', function($scope, $http){
+  $scope.currentPage = 0;
+  $scope.pageSize = 200;
+
+  $scope.setCurrentPage = function(currentPage) {
+    $scope.currentPage = currentPage;
+  }
+
+  function getNumberAsArray(num) {
+    return new Array(num);
+  };
+
+  function numberOfPages() {
+    return Math.ceil($scope.filter_records.length/ $scope.pageSize);
+  };
 
   var httpRequest = $http({
     method: 'GET',
@@ -8,24 +24,29 @@ spendingApp.controller('SpendCtrl', ['$scope', '$http', '$filter', function($sco
   }).success(function(data, status) {
     $scope.filter_records = data;
     $scope.all_records = $scope.filter_records;
+    $scope.total_pages = getNumberAsArray(numberOfPages());
   });
 
   $scope.clear_all = function(){
-    $scope.start_date = '';
-    $scope.end_date = '';
+    $scope.searchRecords = '';
+
+    $scope.start_date  = '';
+    $scope.end_date    = '';
+
     $scope.start_price = '';
-    $scope.end_price = '';
-    $scope.other_view_range();
+    $scope.end_price   = '';
+
+    $scope.date_price_range();
   }
 
-  $scope.other_view_range = function(){
+  $scope.date_price_range = function(){
     var result = [];
 
     var start_date = $scope.start_date ? Date.parse($scope.start_date) : 0;
-    var end_date = $scope.end_date ? Date.parse($scope.end_date) : Date.now();
+    var end_date   = $scope.end_date   ? Date.parse($scope.end_date)   : Date.now();
 
     var start_price = $scope.start_price ? $scope.start_price : 0;
-    var end_price = $scope.end_price ? $scope.end_price : 10000;
+    var end_price   = $scope.end_price   ? $scope.end_price   : 100000;
 
     $.each($scope.all_records, function (index, record){
 
@@ -36,20 +57,26 @@ spendingApp.controller('SpendCtrl', ['$scope', '$http', '$filter', function($sco
         }
       }
     });
+
     $scope.filter_records = result;
+    $scope.setCurrentPage(0);
+    $scope.total_pages = getNumberAsArray(numberOfPages());
   };
 
 }]);
 
-// Old date filtering
-// $scope.view_range = function(record){
-//   var start_date = $scope.start_date ? Date.parse($scope.start_date) : 0;
-//   var end_date = $scope.end_date ? Date.parse($scope.end_date) : Date.now();
+spendingApp.filter('startFrom', function() {
+  return function(input, start) {
+    if(input){
+      return input.slice(start);
+    }
+  }
+});
 
-//   var recordDate = Date.parse(record.date);
-//   if (recordDate >= start_date && recordDate <= end_date){
-//     return true;
-//   }else{
-//     return false;
-//   }
-// }
+spendingApp.directive('watchFilter', [function($scope){
+  return function(scope, element, attrs){
+    if(scope.$last){
+      console.log('hi')
+    }
+  }
+}])
---
+++ public/js/d3_app.js
@@ -0,0 +1,6 @@
+// d3.select("body").style("background-color", "orange");
+// setInterval(function(){
+//   d3.selectAll("p").style("color", function() {
+//     return "hsl(" + Math.random() * 360 + ","+ Math.random() * 100 +"%,"+ Math.random() * 100 +"%)";
+//   });
+// },500)
